52d778b4259efcd071cb2d20667cbea8a9972292
Lye Yi Xian <lyeyx999@hotmail.com>
2023-07-29 17:07:34 +0800

    add health check route

---
+++ app/routes/healthcheck.jsx
@@ -0,0 +1,13 @@
+export const loader = async ({ request }) => {
+  try {
+    const url = new URL(`http://localhost:${process.env.PORT ?? 3000}/`)
+    // if we can make a HEAD request to ourselves, then we're good.
+    await fetch(url.toString(), { method: 'HEAD' }).then((r) => {
+      if (!r.ok) return Promise.reject(r)
+    })
+    return new Response('OK')
+  } catch (error) {
+    console.log('healthcheck ❌', { error })
+    return new Response('ERROR', { status: 500 })
+  }
+}
