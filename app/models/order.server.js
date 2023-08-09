2ad892c1b2ccb4b27a0265a53c0a923e7cbca475
Lye Yi Xian <lyeyx999@hotmail.com>
2023-08-09 21:04:21 +0800

    add api to create and get order

---
+++ app/models/order.server.js
@@ -0,0 +1,44 @@
+import { fetchApi } from '../utils/fetchApi'
+
+export async function createOrder(data) {
+  const { name, phone, enquiry, cartItems } = data
+  const path = '/orders'
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      data: {
+        name,
+        phone,
+        enquiry,
+        products: {
+          connect: [...cartItems.map((item) => item.id)],
+        },
+      },
+    }),
+  }
+  try {
+    return await fetchApi(path, {}, options)
+  } catch {
+    return { error: 'Unable to create order!' }
+  }
+}
+
+export async function getOrder(orderId) {
+  const path = `/orders`
+  const urlParamsObj = {
+    filters: {
+      orderId: {
+        $eq: orderId,
+      },
+    },
+    populate: 'deep',
+  }
+
+  try {
+    const { data } = await fetchApi(path, urlParamsObj)
+
+    return data[0]
+  } catch {
+    return { error: 'Unable to retrieve order!' }
+  }
+}
