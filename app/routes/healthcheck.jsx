34adcd66b158ff724936abae484985e3e87a17a8
Lye Yi Xian <lyeyx999@hotmail.com>
2023-08-12 00:57:07 +0800

    debug prod

--- app/routes/healthcheck.jsx
+++ app/routes/healthcheck.jsx
@@ -1,7 +1,8 @@
 // TODO: deploy failed, cos healthcheck cant pass, probably becos of port error
 export const loader = async ({ request }) => {
   try {
-    const url = new URL(`http://localhost:${process.env.PORT ?? 3000}/`)
+    const url = new URL(`http://localhost:${process.env.PORT || 3000}/`)
+    console.log('--> url', url)
     // if we can make a HEAD request to ourselves, then we're good.
     await fetch(url.toString(), { method: 'HEAD' }).then((r) => {
       if (!r.ok) return Promise.reject(r)
