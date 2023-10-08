b26bd08482bd9bd445606a1932623bc3428cb007
Lye Yi Xian <lyeyx999@hotmail.com>
2023-10-08 21:48:51 +0800

    Add an resource route (api) for add to cart functionality

---
+++ app/routes/api/addToCart.jsx
@@ -0,0 +1,23 @@
+import { json } from '@remix-run/node'
+import { addCart, addToCart } from '../../models/cart.server'
+import { commitSession, getCartId, getSession } from '../../session.server'
+
+export async function action({ request }) {
+  let cartId = await getCartId(request)
+  let options = {}
+
+  if (!cartId) {
+    const session = await getSession(request)
+    const res = await addCart()
+
+    cartId = res.data.attributes.cartId
+    session.set('cartId', cartId)
+    options = { headers: { 'Set-Cookie': await commitSession(session) } }
+  }
+
+  const formData = await request.formData()
+  const productId = formData.get('productId')
+  const res = await addToCart(productId, cartId)
+
+  return json(res, options)
+}
