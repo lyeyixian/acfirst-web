378669df1d1e36f139eae53157b65040af13bd75
Lye Yi Xian <lyeyx999@hotmail.com>
2023-11-01 00:22:32 +0800

    .

---
+++ app/components/product/RelatedProducts.jsx
@@ -0,0 +1,28 @@
+import { Title } from '@mantine/core'
+import AcfirstCarousel from '../common/AcfirstCarousel'
+import { Carousel } from '@mantine/carousel'
+import ProductImageCard from '../common/ProductImageCard'
+
+export default function RelatedProducts({ products }) {
+  const relatedProductsCarousel = products.map((relatedProduct, index) => (
+    <Carousel.Slide key={index}>
+      <ProductImageCard product={relatedProduct} height={225} />
+    </Carousel.Slide>
+  ))
+  return (
+    <>
+      <Title mt={32} order={3}>
+        Related Products
+      </Title>
+      <AcfirstCarousel
+        mt="md"
+        loop
+        slideSize="25%"
+        slidesToScroll="auto"
+        slideGap="md"
+      >
+        {relatedProductsCarousel}
+      </AcfirstCarousel>
+    </>
+  )
+}
