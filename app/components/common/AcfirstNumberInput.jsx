aa11a0e4d7e3aa768f5c18f9d2330846edda1c4a
Lye Yi Xian <lyeyx999@hotmail.com>
2023-11-07 21:30:45 +0800

    Update looks of number input

--- app/components/common/AcfirstNumberInput.jsx
+++ app/components/common/AcfirstNumberInput.jsx
@@ -1,13 +1,34 @@
-import { NumberInput } from '@mantine/core'
+import { ActionIcon, Group, NumberInput, rem } from '@mantine/core'
+import { useRef } from 'react'
 
 export default function AcfirstNumberInput() {
+  const handlers = useRef()
+
   return (
-    <NumberInput
-      name="quantity"
-      my="xs"
-      type="number"
-      min={1}
-      defaultValue={1}
-    />
+    <Group spacing={5}>
+      <ActionIcon
+        size={36}
+        variant="default"
+        onClick={() => handlers.current.decrement()}
+      >
+        -
+      </ActionIcon>
+      <NumberInput
+        hideControls
+        name="quantity"
+        type="number"
+        min={1}
+        defaultValue={1}
+        handlersRef={handlers}
+        styles={{ input: { width: rem(54), textAlign: 'center' } }}
+      />
+      <ActionIcon
+        size={36}
+        variant="default"
+        onClick={() => handlers.current.increment()}
+      >
+        +
+      </ActionIcon>
+    </Group>
   )
 }
