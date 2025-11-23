eae912ace8e3d9f8fb991267d3220ec38929c4c0
Lye Yi Xian <lyeyx999@hotmail.com>
2025-11-23 12:37:38 +0800

    fix: node fetch error when upgrading node to 18

--- app/utils/api/helper.js
+++ app/utils/api/helper.js
@@ -1,5 +1,5 @@
 export function getStrapiURL(path = '') {
-  return `${process.env.STRAPI_URL_BASE || 'http://localhost:1337'}${path}`
+  return `${process.env.STRAPI_URL_BASE || 'http://127.0.0.1:1337'}${path}`
 }
 
 export function getStrapiMedia(img) {
